e29bd2e1dc54801fc741f9c07ca1e754faa2f39a
Kadir Osma <kadirosma24@gmail.com>
2018-09-08 20:06:58 +0300

    move dijkstra and shortest functions to graph class

--- Graph.py
+++ Graph.py
@@ -1,3 +1,5 @@
+import heapq
+
 from Vertex import Vertex
 
 
@@ -38,3 +40,57 @@ class Graph:
 
     def get_previous(self):
         return self.previous
+
+    def dijkstra(self, start_id):
+        print '''Dijkstra's shortest path'''
+
+        # Init variables
+        start = self.get_vertex(start_id)
+
+        # Set the distance for the start node to zero
+        start.set_distance(0)
+
+        # Put tuple pair into the priority queue
+        unvisited_queue = [(v.get_distance(), v) for v in self]
+        heapq.heapify(unvisited_queue)
+
+        while len(unvisited_queue):
+            # Pops a vertex with the smallest distance
+            uv = heapq.heappop(unvisited_queue)
+            current = uv[1]
+            current.set_visited()
+
+            # for next in v.adjacent:
+            for next_vertex in current.adjacent:
+                # if visited, skip
+                if next_vertex.visited:
+                    continue
+                new_dist = current.get_distance() + current.get_weight(next_vertex)
+
+                if new_dist < next_vertex.get_distance():
+                    next_vertex.set_distance(new_dist)
+                    next_vertex.set_previous(current)
+                    print 'updated : current = %s next = %s new_dist = %s' \
+                          % (current.get_id(), next_vertex.get_id(), next_vertex.get_distance())
+                else:
+                    print 'not updated : current = %s next = %s new_dist = %s' \
+                          % (current.get_id(), next_vertex.get_id(), next_vertex.get_distance())
+
+            # Rebuild heap
+            # 1. Pop every item
+            while len(unvisited_queue):
+                heapq.heappop(unvisited_queue)
+            # 2. Put all vertices not visited into the queue
+            unvisited_queue = [(v.get_distance(), v) for v in self if not v.visited]
+            heapq.heapify(unvisited_queue)
+
+    def shortest(self, target_id, path=None):
+        """ make shortest path from v.previous"""
+        v = self.get_vertex(target_id)
+        if path is None:
+            path = [v.get_id()]
+
+        if v.previous:
+            path.append(v.previous.get_id())
+            self.shortest(v.previous.get_id(), path)
+        return path
--- main.py
+++ main.py
@@ -1,62 +1,4 @@
-import heapq
-
 from Graph import Graph
-
-
-def shortest(target_id, path=None):
-    ''' make shortest path from v.previous'''
-    v = g.get_vertex(target_id)
-    if path==None:
-        path = [v.get_id()]
-
-    if v.previous:
-        path.append(v.previous.get_id())
-        shortest(v.previous.get_id(), path)
-    return path
-
-
-def dijkstra(d_g, start_id):
-    print '''Dijkstra's shortest path'''
-
-    # Init variables
-    start = g.get_vertex(start_id)
-
-    # Set the distance for the start node to zero
-    start.set_distance(0)
-
-    # Put tuple pair into the priority queue
-    unvisited_queue = [(v.get_distance(), v) for v in d_g]
-    heapq.heapify(unvisited_queue)
-
-    while len(unvisited_queue):
-        # Pops a vertex with the smallest distance
-        uv = heapq.heappop(unvisited_queue)
-        current = uv[1]
-        current.set_visited()
-
-        # for next in v.adjacent:
-        for next in current.adjacent:
-            # if visited, skip
-            if next.visited:
-                continue
-            new_dist = current.get_distance() + current.get_weight(next)
-
-            if new_dist < next.get_distance():
-                next.set_distance(new_dist)
-                next.set_previous(current)
-                print 'updated : current = %s next = %s new_dist = %s' \
-                      % (current.get_id(), next.get_id(), next.get_distance())
-            else:
-                print 'not updated : current = %s next = %s new_dist = %s' \
-                      % (current.get_id(), next.get_id(), next.get_distance())
-
-        # Rebuild heap
-        # 1. Pop every item
-        while len(unvisited_queue):
-            heapq.heappop(unvisited_queue)
-        # 2. Put all vertices not visited into the queue
-        unvisited_queue = [(v.get_distance(), v) for v in d_g if not v.visited]
-        heapq.heapify(unvisited_queue)
 
 if __name__ == '__main__':
 
@@ -183,9 +125,10 @@ if __name__ == '__main__':
             wid = w.get_id()
             print '( %s , %s, %3d)' % (vid, wid, v.get_weight(w))
 
-    dijkstra(g, '1')
+    g.dijkstra('1')
+
     # dijkstra calculated all distances from the starting point 1
 
-    path = shortest('26')
+    path = g.shortest('26')
 
     print 'The shortest path : %s' % (path[::-1])
